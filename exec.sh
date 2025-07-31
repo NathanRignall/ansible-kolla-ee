31bd363f6b608151f28cfd7ceb83edb84fc5038e
Nathan Rignall <rignallnathan@gmail.com>
2025-07-31 09:47:49 +0100

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
+#
+# This script runs Ansible inside a custom Docker container
+# that has the correct user pre-configured.
+#
 
-IMAGE="nathanrignall/kolla-ansible-ee"
+set -e
 
-USER_ID=$(id -u)
-GROUP_ID=$(id -g)
-USER_NAME="customuser"
-GROUP_NAME="customgroup"
+# --- Configuration ---
+# The name of the custom image created by the build script.
+# Make sure this matches the name in your build_custom_image.sh script.
+CUSTOM_IMAGE_NAME="my-kolla-ansible-ee"
 
-# Run everything in a single container
-# Create user, then use 'sudo' to switch to that user
-docker run -t --rm -it --user root \
+# --- Pre-flight Check ---
+# Check if the custom Docker image exists.
+# If not, exit with instructions to build it first.
+if ! docker image inspect "${CUSTOM_IMAGE_NAME}" &> /dev/null; then
+  echo "Error: Custom Docker image '${CUSTOM_IMAGE_NAME}' not found."
+  echo "Please run the './build_custom_image.sh' script first to build it."
+  exit 1
+fi
+
+echo "Found custom image: ${CUSTOM_IMAGE_NAME}"
+echo "Running Ansible command..."
+echo "-------------------------------------"
+
+# --- Execution ---
+# Run the ansible command inside the custom container.
+# This command is much simpler because the user is already set up in the image.
+# It passes all script arguments ($@) to the ansible command inside the container.
+docker run -it --rm \
   -v "/home/cloud/kolla-config:/workdir" \
   -v "/home/cloud/openstack:/etc/openstack" \
   -v "${SSH_AUTH_SOCK}:/ssh-agent" \
   -e SSH_AUTH_SOCK="/ssh-agent" \
-  "${IMAGE}" /bin/bash -c "
-    groupadd -g ${GROUP_ID} ${GROUP_NAME} &>/dev/null || true
-    useradd -u ${USER_ID} -g ${GROUP_ID} -d /workdir ${USER_NAME} &>/dev/null || true
-    # Add the new user to the sudoers file to allow passwordless sudo
-    echo '${USER_NAME} ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-    # Use 'sudo' to execute the final command as the new user
-    exec sudo -E -u ${USER_NAME} -- \"\$@\"
-  " bash "$@"
+  "${CUSTOM_IMAGE_NAME}" "$@"
